0e64c14519c6d43bb88222c03f0ad073a45a98c9
Ravi Netravali <ravinet@mit.edu>
2015-12-14 17:37:41 -0500

    include long/lat gps fields in POST request

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def run_post():
     with open("./data/cat.jpg", "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     url = 'http://localhost:5000/android_post'
-    data = {'filename': 'cat.jpg', 'image':encoded_string}
+    data = {'filename': 'cat.jpg', 'image':encoded_string, 'longitude': 38.436236, 'latitude': 88.36236236}
     headers = {'Content-Type': 'application/json'}
 
     r = requests.post(url, data=json.dumps(data), headers=headers)
